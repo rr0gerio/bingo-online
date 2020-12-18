6bb96761a1baa92b13c32f0db96ea49007c44450
Rogerio Santos <rogeriosoares01@hotmail.com>
2020-12-18 19:57:42 -0300

    teste

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
-// document.addEventListener('contextmenu', function(e) {
-//     e.preventDefault();
-//   });
-// document.onkeydown = function(e) {
-//     if(e.keyCode == 123) {
-//         return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//         return false;
-//     }
-//     if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//         return false;
-//     }
-// }
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
+document.onkeydown = function(e) {
+    if(e.keyCode == 123) {
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
 function verificaPosicao(arrayPosition){
     //trata a posicao
     arrayPosition = (arrayPosition + 1);
